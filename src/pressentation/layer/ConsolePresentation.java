e15655cc21bc276c1ad26f2da052f9a7ffeca5ee
Wessel Scholtz <576363@student.belgiumcampus.ac.za>
2021-02-26 00:48:16 +0200

    Ground work on P2 presentation

---
+++ src/pressentation/layer/ConsolePresentation.java
@@ -0,0 +1,8 @@
+package pressentation.layer;
+
+public static class ConsolePresentation {
+    public static void intro() {
+        System.out.println("Welcome to D- C-"); //cant remember what we are doing
+        System.out.println("This is the thingy thing thing");
+    }
+}
