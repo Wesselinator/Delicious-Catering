e8a92a08ecfd9316d35f7185547259f010002395
Wessel Scholtz <576363@student.belgiumcampus.ac.za>
2021-02-26 10:06:37 +0200

    Im using contains on one f the objects, equals might be required

--- src/businesslogic/layer/DCClient.java
+++ src/businesslogic/layer/DCClient.java
@@ -39,4 +39,43 @@ public class DCClient {
     public String toString() {
         return "First Name=" + fname + ", Last Name=" + lname + ", Phone Number=" + number;
     }
+
+
+    //auto gen
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((fname == null) ? 0 : fname.hashCode());
+        result = prime * result + ((lname == null) ? 0 : lname.hashCode());
+        result = prime * result + ((number == null) ? 0 : number.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DCClient other = (DCClient) obj;
+        if (fname == null) {
+            if (other.fname != null)
+                return false;
+        } else if (!fname.equals(other.fname))
+            return false;
+        if (lname == null) {
+            if (other.lname != null)
+                return false;
+        } else if (!lname.equals(other.lname))
+            return false;
+        if (number == null) {
+            if (other.number != null)
+                return false;
+        } else if (!number.equals(other.number))
+            return false;
+        return true;
+    }
 }
