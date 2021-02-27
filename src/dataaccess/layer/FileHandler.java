6a71191a64e88ba127f04ed97740f39344682c37
USS-Engineer <marco.venter.net@gmail.com>
2021-02-27 20:15:08 +0200

    Created FileHandler Class

---
+++ src/dataaccess/layer/FileHandler.java
@@ -0,0 +1,17 @@
+package dataaccess.layer;
+
+import java.io.*;
+import java.util.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import businesslogic.layer.DCBooking;
+import businesslogic.layer.DCClient;
+
+public class FileHandler {
+
+    public FileHandler() {}
+
+    
+    
+}
