45b3f0ffc6292a272ee9ddbb5d90935c32444067
USS-Engineer <marco.venter.net@gmail.com>
2021-02-27 20:17:08 +0200

    Added saving/writing functionality

--- src/dataaccess/layer/FileHandler.java
+++ src/dataaccess/layer/FileHandler.java
@@ -12,6 +12,30 @@ public class FileHandler {
 
     public FileHandler() {}
 
-    
+    public void saveBookings(ArrayList<DCBooking> bookings) {
+        ArrayList<DCBooking> data = bookings;         
+        try {
+            Path filepath = Paths.get(System.getProperty("user.dir")+"/dataaccess/layer/bookings.ser");
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filepath.toFile()));
+            out.writeObject(data);
+            out.close();
+            System.out.printf("All bookings have been saved.");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public void saveRegistrations(ArrayList<DCClient> registrations) {
+        ArrayList<DCClient> data = registrations;         
+        try {
+            Path filepath = Paths.get(System.getProperty("user.dir")+"/dataaccess/layer/registrations.ser");
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filepath.toFile()));
+            out.writeObject(data);
+            out.close();
+            System.out.printf("All registrations have been saved.");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
     
 }
