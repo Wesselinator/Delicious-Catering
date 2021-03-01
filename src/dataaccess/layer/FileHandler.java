1ece63e409f04ce9870d8c7f6864c9991aeb29be
USS-Engineer <marco.venter.net@gmail.com>
2021-03-01 10:45:39 +0200

    Implemented Try-with-Resources

--- src/dataaccess/layer/FileHandler.java
+++ src/dataaccess/layer/FileHandler.java
@@ -2,8 +2,6 @@ package dataaccess.layer;
 
 import java.io.*;
 import java.util.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import businesslogic.layer.DCBooking;
 import businesslogic.layer.DCClient;
@@ -14,12 +12,9 @@ public class FileHandler {
 
     public void writeBookings(ArrayList<DCBooking> bookings) {
         ArrayList<DCBooking> data = bookings;         
-        try {
-            Path filepath = Paths.get("Data/bookings.ser");//file will be created if it does not yet exist
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filepath.toFile()));
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Data/bookings.ser"))){          
             out.writeObject(data);
-            out.close();
-            System.out.printf("All bookings have been saved.");
+            System.out.printf("All bookings have been saved.");//This message can also be implemented when the method is used.
         } catch (IOException i) {
             i.printStackTrace();
         }
@@ -27,12 +22,9 @@ public class FileHandler {
 
     public void writeRegistrations(ArrayList<DCClient> registrations) {
         ArrayList<DCClient> data = registrations;         
-        try {
-            Path filepath = Paths.get("Data/registrations.ser");//file will be created if it does not yet exist
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filepath.toFile()));
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Data/registrations.ser"))){          
             out.writeObject(data);
-            out.close();
-            System.out.printf("All registrations have been saved.");
+            System.out.printf("All registrations have been saved.");//This message can also be implemented when the method is used.
         } catch (IOException i) {
             i.printStackTrace();
         }
@@ -40,12 +32,9 @@ public class FileHandler {
 
     public ArrayList<DCBooking> readBookings() {
         ArrayList<DCBooking> data = null;
-        try {
-            Path filepath = Paths.get("Data/bookings.ser");
-            ObjectInputStream input = new ObjectInputStream(new FileInputStream(filepath.toFile()));
+        try (ObjectInputStream input = new ObjectInputStream(new FileInputStream("Data/bookings.ser"));){          
             //@SuppressWarnings("unchecked") //ArrayList needs to be redeclared after this but then return cannot see the list
-            data = (ArrayList<DCBooking>) input.readObject();
-            input.close();          
+            data = (ArrayList<DCBooking>) input.readObject();        
         } catch (IOException i) {
             i.printStackTrace();
         } catch (Exception e) {
@@ -59,12 +48,9 @@ public class FileHandler {
     
     public ArrayList<DCClient> readRegistrations() {
         ArrayList<DCClient> data = null;
-        try {
-            Path filepath = Paths.get("Data/registrations.ser");
-            ObjectInputStream input = new ObjectInputStream(new FileInputStream(filepath.toFile()));
+        try (ObjectInputStream input = new ObjectInputStream(new FileInputStream("Data/registrations.ser"))){          
             //@SuppressWarnings("unchecked") //ArrayList needs to be redeclared after this but then return cannot see the list
-            data = (ArrayList<DCClient>) input.readObject();
-            input.close();          
+            data = (ArrayList<DCClient>) input.readObject();         
         } catch (IOException i) {
             i.printStackTrace();
         } catch (Exception e) {
