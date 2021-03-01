292f42add50c84207e328d39b86b06e07e925ff9
Wesselinator <Wesselinator@users.noreply.github.com>
2021-03-01 18:22:52 +0200

    Remove Presentation layer
    This is part of Project 2 and should not be included

--- src/pressentation/layer/App.java
+++
@@ -1,8 +0,0 @@
-import pressentation.layer.ConsolePresentation;
-import static pressentation.layer.ShortConsoleMethods.*;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        pl("as");
-    }
-}
