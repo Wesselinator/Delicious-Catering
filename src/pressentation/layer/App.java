ae358b6d5337248fb4aa0fa8473e11a22b6010d8
Wessel Scholtz <576363@student.belgiumcampus.ac.za>
2021-03-05 21:43:44 +0200

    Nice splash on main menu

--- src/pressentation/layer/App.java
+++ src/pressentation/layer/App.java
@@ -4,9 +4,10 @@ import pressentation.layer.controls.*;
 import pressentation.layer.menu.ConsoleMenu;
 import pressentation.layer.menu.MenuItem;
 import businesslogic.layer.*;
+import static pressentation.layer.ShortConsoleMethods.*;
 
-//TODO: Double check requirements
 public class App {
+    private static final String DCART = " ___      ___  _      ____   __  ____  ___   __ __  _____        __   ____  ______    ___  ____   ____  ____    ____ \r\n|   \\    /  _]| |    |    | /  ]|    |/   \\ |  |  |/ ___/       /  ] /    ||      |  /  _]|    \\ |    ||    \\  /    |\r\n|    \\  /  [_ | |     |  | /  /  |  ||     ||  |  (   \\_       /  / |  o  ||      | /  [_ |  D  ) |  | |  _  ||   __|\r\n|  D  ||    _]| |___  |  |/  /   |  ||  O  ||  |  |\\__  |     /  /  |     ||_|  |_||    _]|    /  |  | |  |  ||  |  |\r\n|     ||   [_ |     | |  /   \\_  |  ||     ||  :  |/  \\ |    /   \\_ |  _  |  |  |  |   [_ |    \\  |  | |  |  ||  |_ |\r\n|     ||     ||     | |  \\     | |  ||     ||     |\\    |    \\     ||  |  |  |  |  |     ||  .  \\ |  | |  |  ||     |\r\n|_____||_____||_____||____\\____||____|\\___/  \\__,_| \\___|     \\____||__|__|  |__|  |_____||__|\\_||____||__|__||___,_|";
     public static void main(String[] args) {
         Bookings allBookings = new Bookings();
         Registrations allClients = new Registrations();
@@ -15,7 +16,8 @@ public class App {
         Controls clientControls = new ClientControls(allBookings, allClients);
 
 
-        //TODO: introduce
+        System.out.println(DCART);
+        pl("Welcome!"); //or someting
 
         ConsoleMenu MainMenu = new ConsoleMenu();
         MainMenu.add(new MenuItem("Admin", adminControls::login));
